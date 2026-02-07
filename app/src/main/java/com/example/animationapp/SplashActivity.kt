22e92c46b87c5956d815c5e510dbded3aa41dec6
Sagor-Raja <sagorraja11@gmail.com>
2026-02-07 18:08:39 +0600

    Animation App

---
+++ app/src/main/java/com/example/animationapp/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.animationapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val logo = findViewById<ImageView>(R.id.logoImage)
+        val appName = findViewById<TextView>(R.id.appName)
+
+        val logoAnim = AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+        val textAnim = AnimationUtils.loadAnimation(this, R.anim.text_anim)
+
+        logo.startAnimation(logoAnim)
+        appName.startAnimation(textAnim)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SplashActivity::class.java))
+            finish()
+        }, 2500)
+    }
+}
